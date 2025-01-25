577b2dad3e63a463b6f22bfabc47b99102f11016
nixnoob <nixnoob50@gmail.com>
2025-01-25 14:52:34 -0800

    added code comments

---
+++ phone_sync_photos.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+
+# Create temporary mount directory
+mkdir $HOME/tmp_phone
+# Mount using go-mtpfs
+go-mtpfs $HOME/tmp_phone &
+
+# Pause to give dir a chance to mount
+sleep 2
+
+# Sync photos - Change source and destination to fit your needs
+# Note, you need to escape any spaces in the source and destination directories
+rsync -av --progress $HOME/tmp_phone/Internal\ shared\ storage/DCIM/Camera/ $HOME/Pictures/phone/
+
+# Unmount temp directory
+fusermount -u $HOME/tmp_phone
+# Remove temp directory
+rmdir $HOME/tmp_phone
+
+
